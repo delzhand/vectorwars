c86d93e8547d7c2fddd1dd428963b4ed408295d1
Derek Adams <derek@alloymagnetic.com>
2019-09-14 15:54:15 -0400

    cleanup removed code

--- Assets/Scripts/API/APIManager.cs
+++ Assets/Scripts/API/APIManager.cs
@@ -23,13 +23,6 @@ public class APIManager : MonoBehaviour
         baseUrl = offline ? offlineUrl : onlineUrl;
     }
 
-    //public void VersionCheck()
-    //{
-    //    Console.Log("Checking version...");
-    //    ShowFullScreenIndicator();
-    //    StartCoroutine(CheckForUpdates());
-    //}
-
     public void Request(string target, bool fullScreenIndicator = false)
     {
         if (fullScreenIndicator)
@@ -71,111 +64,6 @@ public class APIManager : MonoBehaviour
             }
         }
     }
-
-    //public IEnumerator CheckForUpdates()
-    //{
-    //    string currentVersion = PlayerPrefs.GetString("version_id", "1");
-    //    string requestUrl = baseUrl + "/api/v1/get-updates/" + currentVersion;
-    //    UnityWebRequest www = UnityWebRequest.Get(requestUrl);
-    //    Console.Log("Contacting " + requestUrl);
-    //    yield return www.SendWebRequest();
-
-    //    if (www.isNetworkError || www.isHttpError)
-    //    {
-    //        Console.Log("Communication error: " + www.error);
-    //        finish();
-    //        failure();
-    //    }
-    //    else
-    //    {
-    //        PlayerPrefs.SetString("pending_updates", www.downloadHandler.text);
-    //        finish();
-    //        success();
-    //    }
-    //}
-
-    //public IEnumerator GetUpdate(int update_id)
-    //{
-
-    //}
-
-    //public IEnumerator DownloadFile(string url, string savePath)
-    //{
-    //    UnityWebRequest www = UnityWebRequest.Get(url);
-    //    yield return www.SendWebRequest();
-
-    //    if (www.isNetworkError || www.isHttpError)
-    //    {
-    //        Debug.Log(www.error);
-    //    }
-    //    else
-    //    {
-    //        try
-    //        {
-    //            File.WriteAllBytes(savePath, www.downloadHandler.data);
-    //            Console.Log(url + " downloaded.");
-    //        }
-    //        catch (Exception e)
-    //        {
-    //            Console.Log(e.Message);
-    //        }
-    //    }
-
-    //}
-
-    //public IEnumerator GetVectorCores()
-    //{
-    //    Console.Log("Downloading...");
-    //    UnityWebRequest www = UnityWebRequest.Get("http://vector-wars.lndo.site/api/v1/list-cores");
-    //    yield return www.SendWebRequest();
-
-    //    if (www.isNetworkError || www.isHttpError)
-    //    {
-    //        Console.Log(www.error);
-    //    }
-    //    else
-    //    {
-    //        try
-    //        {
-    //            Console.Log("Applying update...");
-    //            if (!Directory.Exists(Application.persistentDataPath + "/GameData"))
-    //            {
-    //                Console.Log("No Game Data Found. Initializing...");
-    //                Directory.CreateDirectory(Application.persistentDataPath + "/GameData");
-    //            }
-    //            File.WriteAllText(Application.persistentDataPath + "/GameData/VectorCores.json", www.downloadHandler.text);
-    //            VectorCoreList vcores = JsonUtility.FromJson<VectorCoreList>(www.downloadHandler.text);
-    //            if (!Directory.Exists(Application.persistentDataPath + "/GameData/Sprites"))
-    //            {
-    //                Directory.CreateDirectory(Application.persistentDataPath + "/GameData/Sprites");
-    //            }
-    //            foreach (VectorCore v in vcores.vector_core_list)
-    //            {
-    //                StartCoroutine(DownloadFile(v.sprite, Application.persistentDataPath + "/GameData/Sprites/" + v.name + "_sprite.png"));
-    //                Console.Log(v.name + " downloaded.");
-    //            }
-    //        }
-    //        catch(Exception e)
-    //        {
-    //            Console.Log(e.Message);
-    //        }
-    //    }
-    //}
-
-    //private bool updateFound(BasicResponse r)
-    //{
-    //    string localVersion = PlayerPrefs.GetString("version", "0.1.0");
-    //    string[] lv = localVersion.Split('.');
-    //    return int.Parse(lv[0]) < r.data_major_version || int.Parse(lv[1]) < r.data_minor_version || int.Parse(lv[2]) < r.patch_version;
-    //}
-
-    //private void doUpdate(BasicResponse r)
-    //{
-    //    Console.Log("Update data available! v" + r.VersionNumber() + " found. Updating...");
-    //    PlayerPrefs.SetString("version", r.VersionNumber());
-    //    StartCoroutine(GetVectorCores());
-    //    Console.Log("Update complete!");
-    //}
 
     public void ShowFullScreenIndicator()
     {
